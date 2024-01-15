517daa80c92d5a81d6fb40b943b486241aacbb70
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-15 11:52:18 -0300

    feat: adicionado componente customizado da "LoginView"

--- ArquiteturaMVVMCApp/Login/LoginViewController.swift
+++ ArquiteturaMVVMCApp/Login/LoginViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    //MARK: - Properts
+    private lazy var loginView: LoginView = {
+        let view = LoginView(frame: .zero)
+        return view
+    }()
+    
+    //MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
