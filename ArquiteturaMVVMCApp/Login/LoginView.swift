88c91e62cb252fc03a9bbc6cc821030d48556386
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-15 11:51:08 -0300

    feat: criado view que ira conter os componentes de tela

---
+++ ArquiteturaMVVMCApp/Login/LoginView.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class LoginView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemMint
+    }
+    
+    @available(*,unavailable)
+    required init?(coder: NSCoder) { nil }
+}
