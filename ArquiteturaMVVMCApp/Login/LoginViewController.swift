c551348a7f328e49a6b19ce61c6f3d1f5c609b91
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-15 12:32:34 -0300

    feat: adicionado protocolo de ajuda para organizacao do codigo

--- ArquiteturaMVVMCApp/Login/LoginViewController.swift
+++ ArquiteturaMVVMCApp/Login/LoginViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-final class LoginViewController: UIViewController {
-    
+final class LoginViewController: UIViewController, ViewCode {
+   
     //MARK: - Properts
     private lazy var loginView: LoginView = {
         let view = LoginView(frame: .zero)
@@ -11,6 +11,18 @@ final class LoginViewController: UIViewController {
     //MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view = loginView
+        commonInit()
+    }
+    
+    //MARK: - Helpers
+    func setupHierarchy() {
+    }
+    
+    func setupConstraints() { }
+    
+    func setupStyle() {
+        self.title = "Login"
         view.backgroundColor = .systemMint
     }
 }
