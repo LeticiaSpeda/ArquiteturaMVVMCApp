d1eebd6a836a9ca2d8982b26b9dc2bc4cb732f74
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-16 13:02:30 -0300

    feat: criado classe para decidir qual provider chamar

--- ArquiteturaMVVMCApp/Integration/Business/UserBusiness.swift
+++ ArquiteturaMVVMCApp/Integration/Business/UserBusiness.swift
@@ -1,8 +1,40 @@
-//
-//  UserBusiness.swift
-//  ArquiteturaMVVMCApp
-//
-//  Created by Leticia Speda on 16/01/24.
-//
-
 import Foundation
+
+protocol UserBusinessProtocol {
+    func login(email: String, password: String, completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+    func register(email: String, password: String, completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+}
+
+final class UserBusiness: UserBusinessProtocol {
+    private let provider = UserProvider()
+    
+    func login(email: String, password: String, completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
+        
+        provider.login(parameters: getParams( email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHadler(.success(userModel))
+            case .failure(let error):
+                completionHadler(.failure(error))
+            }
+        }
+    }
+    
+    func register(email: String, password: String, completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
+        
+        provider.register(parameters: getParams(email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHadler(.success(userModel))
+            case .failure(let error):
+                completionHadler(.failure(error))
+            }
+        }
+    }
+    
+    private func getParams(_ email: String, _ password: String) -> [AnyHashable: Any] {
+        let userModel = UserModel(email: email, password: password)
+        let params: [AnyHashable: Any] = [Constants.ParametersKeys.body: [Constants.ParametersKeys.userModel: userModel]]
+        return params
+    }
+}
