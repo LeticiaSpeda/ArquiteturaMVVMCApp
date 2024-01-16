f07e31c5e0b5f61c1907b57b7674e056070a86aa
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-16 13:16:04 -0300

    feat: criado funcao para quando clicar nos botoes acionar a funcao que verifica o login

--- ArquiteturaMVVMCApp/Login/LoginViewController.swift
+++ ArquiteturaMVVMCApp/Login/LoginViewController.swift
@@ -1,10 +1,21 @@
 import UIKit
 
 final class LoginViewController: UIViewController, ViewCode {
-   
+    
+    //MARK: - Closure
+    var onRegisterTap: (() -> Void)?
+    var onLoginSuccess: (() -> Void)?
+    
+    
     //MARK: - Properts
     private lazy var loginView: LoginView = {
         let view = LoginView(frame: .zero)
+        view.onRegisterTap = { [weak self] in
+            self?.onRegisterTap?()
+        }
+        view.onEnterTap = { [weak self] email, password in
+            //            self?.onEnterTap?(email, password)
+        }
         return view
     }()
     
@@ -16,6 +27,21 @@ final class LoginViewController: UIViewController, ViewCode {
     }
     
     //MARK: - Helpers
+    private func openTap(_ email: String, _ passwdord: String) {
+        let manager = UserManager(business: UserBusiness())
+        manager.login(email: email, password: passwdord) { [weak self] userModel in
+            self?.onLoginSuccess?()
+        } failureHandler: { [weak self] error in
+            self?.showMessage("Erro", error.localizedDescription)
+        }
+    }
+    
+    func showMessage(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
     func setupHierarchy() {
     }
     
