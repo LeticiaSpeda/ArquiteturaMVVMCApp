a05da3ac0dddf2af8b42474350e2dc4561d9c3d7
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-17 10:08:26 -0300

    feat: criado componente da tela

--- ArquiteturaMVVMCApp/Home/HomeView.swift
+++ ArquiteturaMVVMCApp/Home/HomeView.swift
@@ -1,8 +1,32 @@
-//
-//  HomeView.swift
-//  ArquiteturaMVVMCApp
-//
-//  Created by Leticia Speda on 17/01/24.
-//
+import UIKit
 
-import Foundation
+final class HomeView: UIView, ViewCode {
+    private lazy var helloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 18)
+        label.enableViewCode()
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+    
+    
+    func setupHierarchy() {
+        addSubview(helloLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            helloLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            helloLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
