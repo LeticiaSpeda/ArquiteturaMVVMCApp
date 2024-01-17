07fb9dde3bcb09fe4866b8e40011f1078e2072d0
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-17 10:07:56 -0300

    feat: adicionado o "HomeView" a HomeViewontroller

--- ArquiteturaMVVMCApp/Home/HomeViewController.swift
+++ ArquiteturaMVVMCApp/Home/HomeViewController.swift
@@ -1,6 +1,17 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    
+    private lazy var homeView: HomeView = {
+        let view = HomeView(frame: .zero)
+        return view
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = homeView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
