47bfad7e476e3723f599b687472a3423f29ee988
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-17 09:43:40 -0300

    feat: adicionando funcao que verifica os dados de registro do usuario

--- ArquiteturaMVVMCApp/ViewModel/UserViewModel.swift
+++ ArquiteturaMVVMCApp/ViewModel/UserViewModel.swift
@@ -26,4 +26,15 @@ final class UserViewModel {
             completion(.failure(error))
         }
     }
+    
+    func setUserFromApi(_ email: String, _ password: String, completion: @escaping (Result<UserViewModel, Error>) -> Void) {
+        
+        let manager = UserManager(business: UserBusiness())
+        manager.register(email: email, password: password) { userModel in
+            completion(.success(UserViewModel(model: userModel)))
+        } failureHandler: { error in
+            completion(.failure(error))
+        }
+    }
 }
+                         
